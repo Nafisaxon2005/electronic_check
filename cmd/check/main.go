cdefead379c28b647469ef0bbb92592fca56a5ab
Nafisaxon Nabixo'jayeva <nababdu1@MBP-Nafisaxon.bbrouter>
2026-04-19 14:25:46 +0500

    feat: логика расчёта комиссии

--- cmd/check/main.go
+++ cmd/check/main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"check/internal"
+	Commission "check/internal/commission"
+	"fmt"
+)
+
+func main() {
+	var amount int
+	var cardType int
+
+	for {
+		fmt.Print("Введите сумму перевода(или 0 для выхода): ")
+		fmt.Scan(&amount)
+
+		if amount == 0 {
+			fmt.Println("Программа завершена.")
+			break
+		}
+
+		if amount < 500 || amount > 15000000 {
+			fmt.Println("Ошибка: сумма должна быть от 500 до 15 000 000 сум")
+			fmt.Println("Пожалуйста, попробуйте еще раз.")
+			continue
+		}
+
+		fmt.Print("Тип карты (1 - Alif, 2 - Другая): ")
+		fmt.Scan(&cardType)
+
+		isAlif := cardType == 1
+		var comm, total = Commission.Calculate(amount, isAlif)
+		internal.PrintReceipt(amount, comm, total)
+
+		break
+	}
+}
--- internal/printer.go
+++ internal/printer.go
@@ -1,1 +1,17 @@
 package internal
+
+import "fmt"
+
+func PrintReceipt(amount int, commission int, total int) {
+	fmt.Println("\n================================")
+	fmt.Println("        ЭЛЕКТРОННЫЙ ЧЕК        ")
+	fmt.Println("           alif mobi           ")
+	fmt.Println("================================")
+	fmt.Printf(" Сумма перевода:  %10d сум\n", amount)
+	fmt.Printf(" Комиссия:        %10d сум\n", commission)
+	fmt.Println("--------------------------------")
+	fmt.Printf(" Итого к оплате:  %10d сум\n", total)
+	fmt.Println("================================")
+	fmt.Println("     Спасибо за перевод!       ")
+	fmt.Println("================================")
+}
